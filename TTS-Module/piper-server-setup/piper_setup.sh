531e41f9b57d11a4ba2e69e951e62f54cdcb039f
Bogdan Marghescu <bogdan.marghescu99@gmail.com>
2024-03-02 00:41:15 +0200

    Created a clearer separation between the GUI and CLI versions of the piper TTS test script

--- TTS-Module/piper-server-setup/piper_setup.sh
+++ TTS-Module/piper-server-setup/piper_setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+if [ -n "$VIRTUAL_ENV" ]; then
+    deactivate
+fi
 apt install python3-venv rubberband-cli
 mkdir ~/piper_tts
 cd ~/piper_tts
@@ -7,7 +10,7 @@ python3 -m venv .venv
 source .venv/bin/activate
 pip install pip setuptools wheel
 git clone https://github.com/rhasspy/piper.git
-cd piper/src/python_run
+cd ~/piper_tts/piper/src/python_run
 pip install -e .
 pip install -r requirements_http.txt
 cd ../../..
