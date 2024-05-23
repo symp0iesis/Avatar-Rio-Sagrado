a8783043565ee2e9c33bb225e93a67f682c2359d
danolivaz <33530312+dnllvrvz@users.noreply.github.com>
2024-05-23 13:59:34 -0300

    Add files via upload

---
+++ Code/TTS-Module/piper-server-setup/piper_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt install python3-venv rubberband-cli
+mkdir ~/piper_tts
+cd ~/piper_tts
+python3 -m venv .venv
+source .venv/bin/activate
+pip install pip setuptools wheel
+git clone https://github.com/rhasspy/piper.git
+cd piper/src/python_run
+pip install -e .
+pip install -r requirements_http.txt
+cd ../../..
