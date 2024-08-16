9e525755a9593c1b0880c1f44f77366f369314e5
Mayowa Osibodu <osibodumayowa@gmail.com>
2024-08-16 22:15:26 +0100

    Setting up run-on-startup feature for Avatar

--- Code/sagrado-loop/launcher.sh
+++ Code/sagrado-loop/launcher.sh
@@ -2,8 +2,8 @@
 # launcher.sh
 
 cd ~/Desktop
-# screen -L -Logfile screenlog.0 -d -m source salto-sagrado/bin/activate && cd Sacred-River-AIvatar/Code/sagrado-loop && python sagrado.py
+screen -L -Logfile screenlog.0 -dm bash -c "source salto-sagrado/bin/activate && cd Sacred-River-AIvatar/Code/sagrado-loop && python sagrado.py"
 
-source salto-sagrado/bin/activate
-cd Sacred-River-AIvatar/Code/sagrado-loop
-python sagrado.py
+# source salto-sagrado/bin/activate
+# cd Sacred-River-AIvatar/Code/sagrado-loop
+# python sagrado.py
