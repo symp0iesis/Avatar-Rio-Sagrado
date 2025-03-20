83c8bf32b1c7cc8cdbe9c3c043024448e96679d4
danolivaz <33530312+dnllvrvz@users.noreply.github.com>
2025-03-20 10:26:30 -0300

    Delete Code directory

--- Code/sagrado-loop/launcher.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# launcher.sh
-
-cd ~/Desktop
-screen -L -Logfile screenlog.0 -dm -S sagrado-screen bash -c "source salto-sagrado/bin/activate && cd Sacred-River-AIvatar/Code/sagrado-loop && echo 'Running launcher.sh ....' && python sagrado.py"
-
-# source salto-sagrado/bin/activate
-# cd Sacred-River-AIvatar/Code/sagrado-loop
-# python sagrado.py
